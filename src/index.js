0685a6fe1d7a63d777c5ce7228e83453596dadba
amir gh <amirghasemzadeh17@gmail.com>
2021-11-11 20:24:16 +0330

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { GithubProvider } from "./context/context";
+import { Auth0Provider } from "@auth0/auth0-react";
+// dev-zsazw60h.us.auth0.com
+// 3Z5OQ442msZeh4yglKpBm9XoCUwMvOt9
+ReactDOM.render(
+  <React.StrictMode>
+    <Auth0Provider
+      domain="dev-zsazw60h.us.auth0.com"
+      clientId="3Z5OQ442msZeh4yglKpBm9XoCUwMvOt9"
+      redirectUri={window.location.origin}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
